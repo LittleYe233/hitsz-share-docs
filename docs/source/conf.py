3df2519c34185da7d333d9c40968cf8e40f5e3ba
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-02-24 20:28:29 +0800

    Modify Read the Docs properties

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Hikaru Share'
+copyright = '2022, hikarushare'
+author = 'Hikaru Share'
 
 release = '0.1'
 version = '0.1.0'
